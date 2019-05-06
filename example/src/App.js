b853871eb781a6412f40e081938825458b589e37
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-05-06 18:17:13 +0200

    Modified the default React example to show a red box

--- example/src/App.js
+++ example/src/App.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { hello } from 'use-anim'
 
 function App() {
-    hello();
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="ball" />
     </div>
   );
 }
