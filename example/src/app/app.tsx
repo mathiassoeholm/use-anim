cade68d889c355730c116da0479039c384a3b71a
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-05-07 14:56:00 +0200

    More elaborate example

--- example/src/app/app.tsx
+++ example/src/app/app.tsx
@@ -6,19 +6,41 @@ import { useAnim, easing } from 'use-anim'
 const App: React.FC = () => {
   const [translation, setTranslation] = useState(0);
   const [rotation, setRotation] = useState(0);
+  const [textAnimValue, setTextAnimValue] = useState(0)
+  const [opacity, setOpacity] = useState(0)
 
   useAnim({
-    duration: 1000,
+    duration: 2000,
     updateFunc: (t) => {
-      setTranslation(t*200)
-      setRotation(t*30)
+      setTranslation(t*400)
+      setRotation(t*360)
     },
     playMode: 'pingPong',
     easing: easing.easeInOutQuad,
   });
 
+  useAnim({
+    duration: 8000,
+    updateFunc: (t) => {
+      setTextAnimValue(t)
+    },
+  })
+
+  useAnim({
+    duration: 1000,
+    updateFunc: (t) => {
+      setOpacity(t)
+    },
+  })
+
+  const getText = () => {
+    let text = 'This text is being typed by use-anim :-)'
+    return text.slice(0, Math.round(text.length*textAnimValue))
+  }
+
   return (
-    <div className="container">
+    <div className="container" style={{ opacity: opacity }}>
+      <h1>{getText()}</h1>
       <div className="ball" style={{ transform: `translateX(${translation}px) rotate(${rotation}deg)`}}/>
     </div>
   );
